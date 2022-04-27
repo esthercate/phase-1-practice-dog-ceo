57eb233dd1031d1a44bc127b13ad865c9f8ff1f2
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-27 22:49:32 +0300

    completed assigment

--- src/index.js
+++ src/index.js
@@ -2,46 +2,47 @@ console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
-
-
 fetch(imgUrl)
   .then(res => res.json())
-  .then(displayDogImg)
-
-function displayDogImg(dogs) {
-  dogs.message.forEach(dog => {
-    let dogContainer = document.querySelector("#dog-image-container")
-    let img = document.createElement('img')
-    img.src = dog;
-    img.style.height = "300px"
-    dogContainer.appendChild(img)
-  });
-}
-
-
+  .then(data => {
+    data.message.forEach(element => {
+      let dogContainer = document.querySelector("#dog-image-container");
+      let dogImage = document.createElement("img");
+      dogImage.src = element;
+      dogImage.style.height = "300px";
+      dogImage.style.width = "300px";
+      dogImage.style.margin = "10px";
+      dogImage.style.borderRadius = "8px";
+      dogContainer.appendChild(dogImage);
+    });
+  })
 
 // fetch breeds
-
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 fetch(breedUrl)
   .then(res => res.json())
   .then(dogBreed)
 
-function dogBreed(bread) {
-  for (let breed in bread.message) {
-    let list = document.getElementById('dog-breeds')
-    let li = document.createElement('li')
-    li.innerHTML = breed
-    list.appendChild(li)
+function dogBreed(dBreed) {
+  for (let breed in dBreed.message) {
+    let list = document.getElementById('dog-breeds');
+    let li = document.createElement('li');
+    li.innerHTML = breed;
+    list.appendChild(li);
+
+    //Change li color on click event
+    li.addEventListener("click", () => {
+      li.style.color = "Red";
+    })
   }
 }
 
+//Filter functionality
 document.addEventListener("DOMContentLoaded", () => {
   let selectElements = document.querySelector('#breed-dropdown');
   if (selectElements) {
     selectElements.addEventListener('change', selected)
-
   }
 
   function selected() {
@@ -50,18 +51,20 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(res => res.json())
       .then(dogBreedCode)
 
-    function dogBreedCode(bread) {
+    function dogBreedCode(dBread) {
       console.log(myresult)
       document.getElementById("dog-breeds").innerHTML = '';
-      for (let breed in bread.message) {
+      for (let breed in dBread.message) {
         if (breed.charAt(0) === myresult.charAt(0)) {
-          let lists = document.getElementById('dog-breeds')
-          let lis = document.createElement('li')
-          lis.innerHTML = breed
-          console.log(breed)
-          lists.appendChild(lis)
+          let lists = document.getElementById('dog-breeds');
+          let li = document.createElement('li');
+          li.innerHTML = breed;
+          console.log(breed);
+          lists.appendChild(li);
+          li.addEventListener("click", () => {
+            li.style.color = "Red";
+          })
         }
-
       }
     }
   }
